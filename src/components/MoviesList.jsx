cc9808b9d08e1783092df0959f4f2408a9d1f9c8
Abdul Wajid <abdulwajidnv@gmail.com>
2023-07-10 21:33:01 +0530

    feat: add no movies alert

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -1,5 +1,5 @@
 // Library imports
-import { Grid } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 
 // Local imports
 import MovieCard from "./MovieCard";
@@ -7,17 +7,29 @@ import MovieCard from "./MovieCard";
 const MoviesList = ({ movies }) => {
   return (
     <Grid container spacing={2} py={3}>
-      {movies.map((movie) => (
-        <Grid key={movie.id} item xs={12} sm={6} md={4} xl={3}>
-          <MovieCard
-            title={movie.title}
-            description={movie.description}
-            imageUrl={movie.imageUrl}
-            rating={movie.rating}
-            releaseYear={movie.releaseYear || 0}
-          />
-        </Grid>
-      ))}
+      {movies.length > 0 ? (
+        movies.map((movie) => (
+          <Grid key={movie.id} item xs={12} sm={6} md={4} xl={3}>
+            <MovieCard
+              title={movie.title}
+              description={movie.description}
+              imageUrl={movie.imageUrl}
+              rating={movie.rating}
+              releaseYear={movie.releaseYear || 0}
+            />
+          </Grid>
+        ))
+      ) : (
+        <Typography
+          variant="h4"
+          fontWeight={700}
+          color="#f3f3f3"
+          mx="auto"
+          mt={5}
+        >
+          No Movies Found!
+        </Typography>
+      )}
     </Grid>
   );
 };
